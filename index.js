37e5b5ffa78ace03d8603c5b8893c4f4588e4942
ankitrekha01 <62371794+ankitrekha01@users.noreply.github.com>
2020-11-03 20:11:04 +0530

    added covid-19

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const fetch = require("node-fetch");
-
+const Intl = require('intl');
+const axios = require('axios') 
+//this library is used beacse toLocaleString wasn't working 
 const token = process.env.TOKEN;
 
 const bot = new TelegramBot(token,{
     polling:true
 })
-
-bot.on('text',(message)=>{
-    /* fetch("https://api.rootnet.in/covid19-in/stats/latest")
+     fetch("https://api.rootnet.in/covid19-in/stats/latest")
     .then(res => res.json())
     .then((data)=>{
-        console.log(data)
-    }) */
+        bot.on('text',(message)=>{
+            if(message.text.toLowerCase()=="india" || message.text.toLowerCase()== "ind"){
+                    bot.sendMessage(message.chat.id,`Covid-19 in India
+Total cases   :  ${new Intl.NumberFormat('en-IN').format(parseInt(data.data.summary.total))}
+Deaths           :  ${new Intl.NumberFormat('en-IN').format(parseInt(data.data.summary.deaths))}
+Recovered    :  ${new Intl.NumberFormat('en-IN').format(parseInt(data.data.summary.discharged))}
+                    `);
+            }
+
+            
+            data.data.regional.forEach((loc)=>{
+                if(loc.loc.toLowerCase()=== message.text.toLowerCase()){
+                    bot.sendMessage(message.chat.id,`Covid-19 in ${loc.loc}
+Total cases   :  ${new Intl.NumberFormat('en-IN').format(parseInt(loc.totalConfirmed))}
+Deaths           :  ${new Intl.NumberFormat('en-IN').format(parseInt(loc.deaths))}
+Recovered    :  ${new Intl.NumberFormat('en-IN').format(parseInt(loc.discharged))}
+                    `);
+                }
+            })
+        })
+    }) 
+
+    /* no. of members
     bot.getChatMembersCount(message.chat.id)
-    .then((data)=>console.log(data))    
-})
+    .then((data)=>console.log(data))
+    */ 
