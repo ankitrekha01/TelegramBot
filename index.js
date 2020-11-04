fb1d3b046aa841c2c675a3604d09e6038b60dda4
ankitrekha01 <62371794+ankitrekha01@users.noreply.github.com>
2020-11-04 12:47:23 +0530

    added covid-19 functionality

--- index.js
+++ index.js
@@ -9,21 +9,48 @@ const token = process.env.TOKEN;
 const bot = new TelegramBot(token,{
     polling:true
 })
+
+
      fetch("https://api.rootnet.in/covid19-in/stats/latest")
     .then(res => res.json())
     .then((data)=>{
         bot.on('text',(message)=>{
+            var match=0;
             if(message.text.toLowerCase()=="india" || message.text.toLowerCase()== "ind"){
+                match=1;
                     bot.sendMessage(message.chat.id,`Covid-19 in India
 Total cases   :  ${new Intl.NumberFormat('en-IN').format(parseInt(data.data.summary.total))}
 Deaths           :  ${new Intl.NumberFormat('en-IN').format(parseInt(data.data.summary.deaths))}
 Recovered    :  ${new Intl.NumberFormat('en-IN').format(parseInt(data.data.summary.discharged))}
                     `);
             }
-
-            
+            var m= message.text.toLowerCase()
             data.data.regional.forEach((loc)=>{
+                if(m=="andaman"||m=="andaman&nicobar"||m=="island"){
+                    message.text="Andaman and Nicobar Islands"
+                }
+                else if(m=="andhra"){
+                    message.text = "Andhra Pradesh"
+                }
+                else if(m=="arunachal"){
+                    message.text = "Arunachal Pradesh"
+                }
+                else if(m=="dadra and nagar haveli"||m=="daman and diu"){
+                    message.text = "Dadra and Nagar Haveli and Daman and Diu"
+                }
+                else if(m=='himachal'){
+                    message.text = "Himachal Pradesh"
+                }
+                else if(m=="jammu"||m=="kashmir"||m=="j&k"){
+                    message.text='Jammu and Kashmir'
+                }
+                else if(m=="bengal"){
+                    message.text ="West Bengal"
+                }
+
+
                 if(loc.loc.toLowerCase()=== message.text.toLowerCase()){
+                    match=1;
                     bot.sendMessage(message.chat.id,`Covid-19 in ${loc.loc}
 Total cases   :  ${new Intl.NumberFormat('en-IN').format(parseInt(loc.totalConfirmed))}
 Deaths           :  ${new Intl.NumberFormat('en-IN').format(parseInt(loc.deaths))}
@@ -31,8 +58,13 @@ Recovered    :  ${new Intl.NumberFormat('en-IN').format(parseInt(loc.discharged)
                     `);
                 }
             })
+            if(match!=1){
+                bot.sendMessage(message.chat.id,"Not Found")
+            }
         })
-    }) 
+    })
+    
+    //https://www.googleapis.com/youtube/v3/search?q=hello&key=AIzaSyD3HahrJCN2LAuk8O8xHXbqXVuf4WP9j2g
 
     /* no. of members
     bot.getChatMembersCount(message.chat.id)
